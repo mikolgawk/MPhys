e6f1274cfcd17b6336901ceac8609982c9fd8e49
mikolgawk <miki.gawkowski@gmail.com>
2023-10-20 22:02:30 +0100

    Update mp_band_structures.py

--- mp_band_structures.py
+++ mp_band_structures.py
@@ -13,33 +13,36 @@ from pymatgen.ext.matproj import MPRester
 from pymatgen.electronic_structure.plotter import BSPlotter
 from pymatgen.electronic_structure.bandstructure import BandStructure
 import json
+import os
 
 if __name__ == "__main__":
     MAPI_KEY = "bbiUJxPj5bmp31SqSsuc0EAcO3ekNw6f"  # You must change this to your Materials API key! (or set MAPI_KEY env variable)
     mpr = MPRester(MAPI_KEY)  # object for connecting to MP Rest interface
     
-    for id_ in range(100,102):    
+    for id_ in range(10000,10010):    
         mp_id = "mp-" + str(id_)  # You must change this to the mp-id of your compound of interest
         has_bs = False
         try:
-            bs = mpr.get_bandstructure_by_material_id(mp_id)
+            bs = mpr.get_bandstructure_by_material_id(mp_id) # Get bandstructure by material id
             if bs is not None: has_bs = True
         except:
             pass
         if has_bs:
-            print('Band gap info: {}'.format(bs.get_band_gap()))
-            print(id_)
-            filename = f'band_structure_{mp_id}'
-            BSPlotter(bs).save_plot(filename)
-            
-            bs_dict = bs.as_dict()
-            with open("mp-"+str(id_)+".json", "w") as json_file:
-                json.dump(bs_dict, json_file)
-              
-        
-        
-        #mpr = MPRester(MAPI_KEY)  # object for connecting to MP Rest interface
+            try:
+                print('Band gap info: {}'.format(bs.get_band_gap()))
+                print(id_)
 
-        #my_bs = mpr.get_bandstructure_by_material_id(MP_ID)
-        #print('Band gap info: {}'.format(my_bs.get_band_gap()))
-        #BSPlotter(my_bs).show()
+                filename = f'{mp_id}'
+
+                # Specify the directory where you want to save the JSON file
+                directory = "/home/mikolaj/MPhys_project/band_structures/"
+                json_filename = os.path.join(directory, f"{filename}.json")
+
+
+                BSPlotter(bs).save_plot(filename)
+                bs_dict = bs.as_dict()
+                with open(json_filename, "w") as json_file:
+                    json.dump(bs_dict, json_file, indent=2)
+            except:
+                pass  
+        
